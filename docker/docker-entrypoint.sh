73ea96bb17045ff7b34f24bcaff2370b0c31353f
Joel De Lamarre <jdelamarre@accedian.com>
2017-07-28 12:31:00 -0400

    fixed hostname

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -6,7 +6,10 @@ cp /pithos/doc/pithos.yaml /etc/pithos/
 sed -i \
   -e "s/localhost/cassandra/g" \
   -e "s/level: info/level: debug/" \
+  -e "s/pithos-hostname/${HOSTNAME}/" \
   /etc/pithos/pithos.yaml
+
+
 
 readonly version=$(awk '/\(defproject/{gsub("\"", "", $3);print $3}' project.clj)
 readonly target="/pithos/pithos-${version}-standalone.jar"
